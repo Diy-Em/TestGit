f578f87601aab7622f29c56645206ca0af61c81f
Diy-Em <bondekwe_diy-em_desy.0910.student@ifosupwavre.be>
2019-01-19 10:00:20 +0100

    main

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Coucou");
+        System.out.println("Test de changement");
+        System.out.println("Verife");
+    }
+}
